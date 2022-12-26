3c6f33e8705e7ea220a1dd6ed7a4f55eaa7cd284
Miriam Ruiz <mruiz@espublico.tech>
2022-12-27 00:05:02 +0100

    split restorer and importer

---
+++ importer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export DOTFILES_PATH="$HOME/.dotfiles"
+echo $DOTFILES_PATH
+echo ">"
+echo ">"
+echo ">"
+echo "> Go to import package"
+$DOTLY_PATH/bin/dot package import
+
+echo ">"
+echo ">"
+echo ">"
+echo ">"
+echo ">"
+echo ">"
+echo "> Ejecutamos los post-import"
+sudo systemctl enable lightd.service
+
+echo ">"
+echo ">"
+echo ">"
+read -p "Restauración terminada, presione una tecla para reiniciar.."
+
