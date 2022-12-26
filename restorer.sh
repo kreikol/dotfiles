01816891b8f8cd9e4684769fdd7de527feba1d84
Miriam Ruiz <mruiz@espublico.tech>
2022-12-26 23:19:07 +0100

    Prueba custom restorer

---
+++ restorer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DOTFILES_PATH="$1"
+
+git clone https://github.com/kreikol/prueba $DOTFILES_PATH
+
+cd $DOTFILES_PATH
+
+git submodule update --init --recursive modules/dotly
+
+
+DOTLY_PATH="$DOTFILES_PATH/modules/dotly"
+
+"$DOTLY_PATH/bin/dot" self install
+
+"$DOTLY_PATH/bin/dot" package import
+
+echo "Ejecutamos los post-import"
+sudo systemctl enable lightd.service
+
+echo "Restauración terminada, presione una tecla para reiniciar.."
+
+if whiptail --yesno "Is this a good question" 20 60 ;then
+	sudo reboot
+else
+	echo "Todo listo para trabajar!! :) " 
+fi
