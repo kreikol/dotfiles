bbbc9b8f902fe98f817b1dac28474183a823b84b
Miriam Ruiz <mruiz@espublico.tech>
2022-12-26 23:51:42 +0100

    Fix custom restorer

--- restorer.sh
+++ restorer.sh
@@ -2,18 +2,38 @@
 
 export DOTFILES_PATH="$HOME/.dotfiles"
 echo $DOTFILES_PATH
+echo ">"
+echo ">"
+echo ">"
+echo ">"
+echo ">"
 
 git clone https://github.com/kreikol/prueba $DOTFILES_PATH
 
 cd $DOTFILES_PATH
 
+echo "> $PWD"
+echo ">"
+echo ">"
 git submodule update --init --recursive 
 
 export DOTLY_INSTALLER=true
 export DOTLY_PATH="$DOTFILES_PATH/modules/dotly"
 
+
+echo ">"
+echo $DOTFILES_PATH
+echo $DOTLY_PATH
+echo $DOTLY_INSTALLER
+echo ">"
+echo "> Go to install dotly"
+
 $DOTLY_PATH/bin/dot self install
 
+echo ">"
+echo ">"
+echo ">"
+echo "> Go to import package"
 $DOTLY_PATH/bin/dot package import
 
 echo "Ejecutamos los post-import"
