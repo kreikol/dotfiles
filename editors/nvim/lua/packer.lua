dd7d0305c9f9bb4732db6b180072f516700c0d9c
MiR <kreikol@gmail.com>
2022-12-31 19:49:57 +0000

    Add neovim installer

---
+++ editors/nvim/lua/packer.lua
@@ -0,0 +1,17 @@
+
+-- Plugin manager: packer.nvim
+-- https://github.com/wbthomason/packer.nvim
+
+-- For information about installed plugins see the README
+--- neovim-lua/README.md
+--- https://github.com/brainfucksec/neovim-lua#readme
+
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+-- print("Install path packer", install_path)
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  print("Packer installed")
+end
