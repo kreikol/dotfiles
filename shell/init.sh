700685365994f02048e7a865436d6e6c22f0f986
kreikol <35665933+kreikol@users.noreply.github.com>
2022-12-31 14:02:55 +0100

    Update init.sh
    add finish restore process

--- shell/init.sh
+++ shell/init.sh
@@ -3,3 +3,13 @@
 source "$DOTFILES_PATH/shell/aliases.sh"
 source "$DOTFILES_PATH/shell/exports.sh"
 source "$DOTFILES_PATH/shell/functions.sh"
+
+FICHERO=~/.restore.mir
+if [ -f "$FICHERO" ]
+then
+           echo "Restauracion en proceso, vamos a importar los paquetes"
+           bash <(curl -s https://raw.githubusercontent.com/kreikol/prueba/HEAD/importer.sh)
+              rm -rf $FICHERO
+      else
+                 echo "No hay ninguna restauracion en proceso"
+fi
