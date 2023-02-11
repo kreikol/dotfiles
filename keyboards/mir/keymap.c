d3515b0b9edf1a1209cd4c3e0fd8d7621eabd40f
Miriam Ruiz <kreikol@gmail.com>
2023-02-11 08:07:26 +0100

    Control/Shift/Alt to homing row

--- keyboards/mir/keymap.c
+++ keyboards/mir/keymap.c
@@ -31,9 +31,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      LCTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+      LCTL_T(KC_ESC), LSFT_T(KC_A), KC_S, KC_D, LALT_T(KC_F), KC_G,                KC_H,    KC_J,    KC_K,    KC_L, RSFT_T(KC_SCLN), LCTL_T(KC_QUOT),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      TD(TDCAPS),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  RSFT_T(KC_ENT),
+      TD(TDCAPS),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   MO(1),  LALT_T(KC_SPC),     KC_SPC,  KC_RALT, MO(2)
                                       //`--------------------------'  `--------------------------'
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TILD,  KC_EXLM,  KC_AT,  KC_HASH,    KC_DLR,  KC_PERC,                 KC_CIRC,    KC_AMPR,   KC_ASTR,   KC_LPRN, KC_RPRN, KC_PLUS,
+      KC_TILD,  KC_EXLM,  KC_AT,  KC_HASH,  KC_DLR,  KC_PERC,                    KC_CIRC,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,    KC_PLUS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_GRV,    KC_1,    KC_2,  KC_3,     KC_4,    KC_5,                         KC_6,    KC_7,   KC_8     ,KC_9,     KC_0, KC_MINS,
+      KC_GRV, LSFT_T(KC_1), KC_2,  KC_3,  LALT_T(KC_4),  KC_5,                     KC_6,    KC_7,    KC_8 ,    KC_9,  RSFT_T(KC_0), RCTL_T(KC_MINS),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_EQL, KC_UNDS, //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, _______,  KC_SPC,     KC_RGUI,   MO(3), XXXXXXX
@@ -73,6 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                           KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
   )
+
 };
 
 #ifdef OLED_ENABLE
